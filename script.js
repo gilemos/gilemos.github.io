91a80ec28dd0a9b8d6a3b42b2b344c859677997a
Giovanna Lemos <gigi18377@gmail.com>
2021-08-08 18:12:51 -0400

    changes style of first and second sections

---
+++ script.js
@@ -0,0 +1,26 @@
+var scrollFunc = function() {
+    disDiv = document.getElementById("about-disappearingDiv");
+    skillsDiv = document.getElementById("about-skillsDiv");
+    var y = window.scrollY;
+    if (y >= (window.screen.height * 0.9) && y <  (window.screen.height * 1.7)) {
+        disDiv.style.opacity = 1;
+    } else {
+        disDiv.style.opacity = 0;
+        skillsDiv.style.opacity = 0;
+    }
+
+    if (y >= (window.screen.height) && y < (window.screen.height * 1.7)) {
+        skillsDiv.style.opacity = 1;
+    }
+};
+
+var loadFunc = function() {
+    disDiv = document.getElementById("about-disappearingDiv");
+    padDiv = document.getElementById("about-paddingDiv");
+    padDiv.style.height = disDiv.clientHeight * 2;
+};
+
+window.addEventListener("scroll", scrollFunc);
+window.addEventListener("load", loadFunc);
+
+
